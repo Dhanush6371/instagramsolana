44a4a5d627b2a6fa74292089b3e3c03c9a1e00eb
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-11 10:43:35 +0530

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/";
+
+export const PROGRAM_ID = new PublicKey("ByhYGc3GURKRRfn7rwaqHcTRpwfc4XUdWNZpsdtuf43g")
